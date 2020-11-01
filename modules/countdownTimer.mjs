8b804dbc0d97161ae74f4691bf946e25890ba45d
DaveDevPerry <daveperry.log@gmail.com>
2020-11-01 16:11:20 +0000

    update draw

--- modules/countdownTimer.mjs
+++ modules/countdownTimer.mjs
@@ -25,7 +25,7 @@
 
 export function countdownTimer() {
 	// sat 19:45 wed 20:00
-	const satDrawTimer = new Date('Oct 31, 2020 19:45:00').getTime();
+	const satDrawTimer = new Date('Nov 04, 2020 20:00:00').getTime();
 	const x = setInterval(function () {
 		const now = new Date().getTime();
 		const t = satDrawTimer - now;
