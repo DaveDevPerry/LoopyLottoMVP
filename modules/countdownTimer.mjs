345c47a28a6b620f42d01d54bf7d4e9b8246f157
DaveDevPerry <daveperry.log@gmail.com>
2021-02-13 20:04:06 +0000

    update draw

--- modules/countdownTimer.mjs
+++ modules/countdownTimer.mjs
@@ -25,7 +25,7 @@
 
 export function countdownTimer() {
 	// sat 19:45 wed 20:00
-	const satDrawTimer = new Date('Feb 13, 2021 19:45:00').getTime();
+	const satDrawTimer = new Date('Feb 17, 2021 20:00:00').getTime();
 	const x = setInterval(function () {
 		const now = new Date().getTime();
 		const t = satDrawTimer - now;
