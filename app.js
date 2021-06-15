bd77ae07b880cdf7b2c14b7d30807c1f208c859e
DaveDevPerry <daveperry.log@gmail.com>
2021-06-15 21:30:05 +0100

    basic loader built not sure if using for MVP

--- app.js
+++ app.js
@@ -33,8 +33,14 @@ export let allBallsDrawn;
 // });
 
 window.onload = function () {
-	document.querySelector('#intro').play();
+	setTimeout(() => {
+		document.querySelector('.loader-container').style.display = 'none';
+		document.querySelector('#intro').play();
+	}, 2000);
 };
+// window.onload = function () {
+// 	document.querySelector('#intro').play();
+// };
 document.querySelector('#date').innerText = getDate();
 document.querySelector('#game-number').innerText = gameStartDates.length;
 document.querySelector('#hamburger-unique').addEventListener('click', (e) => {
